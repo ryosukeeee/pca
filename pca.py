c99a07e6c2dc6cb0e171781faeca3c5be98088ff
ryosukeeee <horie-125@asagi.waseda.jp>
2018-02-21 22:48:01 +0900

    svmによる分類の実装

--- pca.py
+++ pca.py
@@ -1,11 +1,13 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt 
-
 from sklearn.datasets import fetch_mldata
+from sklearn import model_selection, svm, metrics
 
 mnist = fetch_mldata('MNIST original', data_home="./")
-mnist_data = mnist.data
+mnist_data = mnist.data /255
 mnist_label = mnist.target
-print(mnist_data)
-print(mnist_label)
+data_train, data_test, label_train, label_test = model_selection.train_test_split(mnist_data, mnist_label, test_size=100, train_size=1000)
+clf = svm.SVC()
+clf.fit(data_train, label_train)
+pre = clf.predict(data_test)
+
+ac_score = metrics.accuracy_score(label_test, pre)
+print(ac_score)

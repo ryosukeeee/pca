f19ca2e74b089d64227386d49e96b2bbc00a0ed2
ryosukeeee <horie-125@asagi.waseda.jp>
2018-02-21 21:27:39 +0900

    値とラベルに分割

--- pca.py
+++ pca.py
@@ -1,2 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+
 from sklearn.datasets import fetch_mldata
-mnist = fetch_mldata('MNIST original', data_home=custom_data_home)
+
+mnist = fetch_mldata('MNIST original', data_home="./")
+mnist_data = mnist.data
+mnist_label = mnist.target
+print(mnist_data)
+print(mnist_label)
